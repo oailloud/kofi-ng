ea31e4f5668355b345c6b74e58d4495240505380
Olivier Ailloud <olivier.ailloud@mikamai.com>
2026-03-31 16:28:57 +0200

    claude: create agent for code formatting and execute it

--- src/app/coffee-list/coffee-list.component.ts
+++ src/app/coffee-list/coffee-list.component.ts
@@ -1,40 +1,37 @@
-import { Component, computed, effect, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, inject, signal } from '@angular/core';
 import { CatalogService } from '../services/catalog';
 import { Coffee } from '../types/coffees';
 import { UpdateForm } from '../update-form/update-form';
 
 @Component({
   selector: 'app-coffee-list',
-  standalone: true,
   imports: [UpdateForm],
   templateUrl: './coffee-list.component.html',
-  styleUrl: './coffee-list.component.scss'
+  styleUrl: './coffee-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CoffeeListComponent {
 
   protected coffees = computed(() => ([
-      ...this.backendCoffees(),
-      {
-        id: '4',
-        name: this.extraCoffee(),
-        description: 'Coffee 4 description',
-        origin: 'Coffee 4 origin',
-        price: 4.0,
-      }
-    ])
-  );
+    ...this.backendCoffees(),
+    {
+      id: '4',
+      name: this.extraCoffee(),
+      description: 'Coffee 4 description',
+      origin: 'Coffee 4 origin',
+      price: 4.0,
+    }
+  ]));
   protected backendCoffees = signal<Coffee[]>([]);
-  extraCoffee = signal('Washed bourbon');
+  protected extraCoffee = signal('Washed bourbon');
 
-  catalog = inject(CatalogService);
+  protected catalog = inject(CatalogService);
 
   constructor() {
     effect(() => {
       console.log(this.backendCoffees());
-    }
-    );
+    });
   }
-
 
   ngOnInit() {
     this.catalog.getCoffees().subscribe((coffees) => {
